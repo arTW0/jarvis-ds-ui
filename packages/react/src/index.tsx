f7bd44eae28ba5cda0876929dfec53c0675fa782
root <root@Arthur>
2023-10-07 21:31:37 -0300

    avatar ready

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,31 +1,4 @@
-import { ComponentProps } from 'react'
-
-import { styled } from './styles'
-
-export const Button = styled('button', {
-  fontFamily: '$default',
-  backgroundColor: '$red300',
-  borderRadius: '$sm',
-  border: 'none',
-  fontWeight: '$bold',
-  color: '$white',
-
-  variants: {
-    size: {
-      small: {
-        fontSize: 14,
-        padding: '$2 $4',
-      },
-      big: {
-        fontSize: 16,
-        padding: '$3 $6',
-      },
-    },
-  },
-
-  defaultVariants: {
-    size: 'small'
-  }
-})
-
-export type ButtonProps = ComponentProps<typeof Button>
+export * from './components/Box'
+export * from './components/Text'
+export * from './components/Heading'
+export * from './components/Avatar'
